1c81460ffc073891e7da1b30265f429b6b3ec743
Timo van de Put <timo.van.de.put@philips.com>
2020-10-08 08:14:45 +0200

    Add properietary license header

--- src/main/java/com/philips/research/spdxbuilder/persistence/package-info.java
+++ src/main/java/com/philips/research/spdxbuilder/persistence/package-info.java
@@ -1,6 +1,11 @@
 /*
- * Copyright (c) 2020-2020, Koninklijke Philips N.V., https://www.philips.com
- * SPDX-License-Identifier: MIT
+ * This software and associated documentation files are
+ *
+ * Copyright © 2020-2020 Koninklijke Philips N.V.
+ *
+ * and is made available for use within Philips and/or within Philips products.
+ *
+ * All Rights Reserved
  */
 
 @pl.tlinkowski.annotation.basic.NonNullPackage
