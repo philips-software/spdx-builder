a4df60580108c00b925287aedf5a3b816961f931
Timo van de Put <timo.van.de.put@philips.com>
2021-06-01 15:15:02 +0200

    Feature: Print BOM tree in internal format

---
+++ src/main/java/com/philips/research/spdxbuilder/persistence/tree/TreeFormatter.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020-2021, Koninklijke Philips N.V., https://www.philips.com
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.philips.research.spdxbuilder.persistence.tree;
+
+public class TreeFormatter {
+    private static final String INDENT = "  ";
+
+    private int indent = 0;
+
+    public String node(String name) {
+        return INDENT.repeat(indent) + name;
+    }
+
+    public void indent() {
+        indent++;
+    }
+
+    public void unindent() {
+        indent --;
+    }
+}
---
+++ src/main/java/com/philips/research/spdxbuilder/persistence/tree/TreeWriter.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2020-2021, Koninklijke Philips N.V., https://www.philips.com
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.philips.research.spdxbuilder.persistence.tree;
+
+import com.github.packageurl.MalformedPackageURLException;
+import com.github.packageurl.PackageURL;
+import com.philips.research.spdxbuilder.core.BomWriter;
+import com.philips.research.spdxbuilder.core.domain.BillOfMaterials;
+import com.philips.research.spdxbuilder.core.domain.Package;
+import com.philips.research.spdxbuilder.core.domain.Relation;
+
+import java.util.*;
+
+/**
+ * Writes the packages of a bill-of-materials as a tree to the console.
+ */
+public class TreeWriter implements BomWriter {
+    private static final String SNIP = "-".repeat(10) + "8<" + "-".repeat(10);
+
+    private final Map<Package, List<Relation>> nodes = new HashMap<>();
+    private final List<Package> roots = new ArrayList<>();
+    private final Set<Package> done = new HashSet<>();
+    private final TreeFormatter formatter;
+
+    public TreeWriter() {
+        this(new TreeFormatter());
+    }
+
+    TreeWriter(TreeFormatter formatter) {
+        this.formatter = formatter;
+    }
+
+    @Override
+    public void write(BillOfMaterials bom) {
+        buildNodes(bom);
+
+        System.out.println("TREE start " + SNIP);
+        roots.forEach(pkg -> {
+            System.out.println(formatter.node(name(pkg)));
+            writeRelationsOf(pkg);
+        });
+        System.out.println("TREE end " + SNIP);
+    }
+
+    private void buildNodes(BillOfMaterials bom) {
+        nodes.clear();
+        bom.getPackages().forEach(pkg -> nodes.put(pkg, new ArrayList<>()));
+
+        roots.clear();
+        roots.addAll(nodes.keySet());
+        roots.sort(Comparator.comparing(this::name));
+
+        bom.getRelations().forEach(rel -> {
+            roots.remove(rel.getTo());
+            nodes.get(rel.getFrom()).add(rel);
+        });
+        nodes.values().forEach(list -> list.sort(Comparator.comparing(rel -> name(rel.getTo()))));
+
+        done.clear();
+        done.addAll(roots);
+    }
+
+    private void writeRelationsOf(Package pkg) {
+        done.add(pkg);
+        final var relations = nodes.get(pkg);
+        if (!relations.isEmpty()) {
+            formatter.indent();
+            relations.forEach(this::writeRelation);
+            formatter.unindent();
+        }
+    }
+
+    private void writeRelation(Relation relation) {
+        Package pkg = relation.getTo();
+        final var name = name(pkg);
+        final var type = type(relation);
+        System.out.println(formatter.node(name + type));
+        if (!done.contains(pkg)) {
+            writeRelationsOf(pkg);
+        }
+    }
+
+    private String type(Relation relation) {
+        switch (relation.getType()) {
+            case DESCENDANT_OF:
+                return " (modified)";
+            case DYNAMIC_LINK:
+                return " (dynamic)";
+            case STATIC_LINK:
+                return " (static)";
+            case DEPENDS_ON:
+            default:
+                return "";
+        }
+    }
+
+    private String name(Package pkg) {
+        return pkg.getPurl().orElseGet(() -> {
+            try {
+                return new PackageURL("generic", pkg.getNamespace(), pkg.getName(), pkg.getVersion(), null, null);
+            } catch (MalformedPackageURLException e) {
+                throw new IllegalArgumentException("Failed to create generic package URL for " + pkg);
+            }
+        }).toString();
+    }
+}
